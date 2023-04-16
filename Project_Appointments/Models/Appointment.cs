2572bd4f81cf331ed2ff44877bb6f06932dfb5e9
Kevin S. Soares <kevin.soares@hotmail.com>
2023-04-16 16:26:09 -0300

    added Appointment model

---
+++ Project_Appointments/Models/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using Project_Appointments.Models.Attributes;
+
+namespace Project_Appointments.Models
+{
+    [AppointmentValidation]
+    public class Appointment
+    {
+        public long Id { get; set; }
+        public long ScheduleId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string PatientName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsPartiallyInserted(Appointment other)
+        {
+            return (other.StartTime <= StartTime && other.EndTime >= StartTime)
+                || (other.StartTime <= EndTime && other.EndTime >= EndTime);
+        }
+
+        public bool IsCompletelyInserted(Appointment other)
+        {
+            return (other.StartTime <= StartTime && other.EndTime >= StartTime)
+                && (other.StartTime <= EndTime && other.EndTime >= EndTime);
+        }
+    }
+}
---
+++ Project_Appointments/Models/Attributes/AppointmentValidationAttribute.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Project_Appointments.Models.Attributes
+{
+    public class AppointmentValidationAttribute : ValidationAttribute
+    {
+        private static readonly string s_regexName = @"^[A-Z][a-z]*(\s[A-Z][a-z]*)+$";
+        private static readonly TimeSpan s_timeout = TimeSpan.FromMilliseconds(200);
+
+        private Appointment _model = default!;
+
+        public override bool IsValid(object? value)
+        {
+            if(value is not Appointment)
+            {
+                ErrorMessage = "Invalid Appointment";
+                return false;
+            }
+            _model = (Appointment) value;
+            return ArePropertiesValid();
+        }
+
+        private bool ArePropertiesValid()
+        {
+            return IsIdValid()
+                && IsScheduleIdValid()
+                && AreTimesValid()
+                && IsPatientNameValid();
+        }
+
+        private bool IsIdValid()
+        {
+            bool condition = _model.Id >= 0L;
+            if(condition is false)
+            {
+                ErrorMessage = "Invalid Appointment.Id";
+            }
+            return condition;
+        }
+
+        private bool IsScheduleIdValid()
+        {
+            bool condition = _model.ScheduleId >= 0L;
+            if (condition is false)
+            {
+                ErrorMessage = "Invalid Appointment.ScheduleId";
+            }
+            return condition;
+        }
+
+        private bool AreTimesValid()
+        {
+            bool condition = _model.StartTime < _model.EndTime;
+            if(condition is false)
+            {
+                ErrorMessage = "Invalid Appointment.StartTime or Appointment.EndTime";
+            }
+            return condition;
+        }
+
+        private bool IsPatientNameValid()
+        {
+            bool condition =
+                Regex.IsMatch(input: _model.PatientName, pattern: s_regexName,
+                    options: RegexOptions.None, matchTimeout: s_timeout);
+
+            if(condition is false)
+            {
+                ErrorMessage = "Invalid Schedule.PatientName";
+            }
+            return condition;
+        }
+    }
+}
