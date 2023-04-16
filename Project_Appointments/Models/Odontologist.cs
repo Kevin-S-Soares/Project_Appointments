96700c35b6bcd1615b727b8bfe42da22061f5dea
Kevin S. Soares <kevin.soares@hotmail.com>
2023-04-16 16:46:15 -0300

    added Odontologist model

---
+++ Project_Appointments/Models/Odontologist.cs
@@ -0,0 +1,13 @@
+﻿using Appointments_Project.Models.Attributes;
+
+namespace Project_Appointments.Models
+{
+    [OdontologistValidation]
+    public class Odontologist
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
