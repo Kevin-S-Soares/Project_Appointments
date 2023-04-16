fb139398fbecab533192a86795b18e86a198c14c
Kevin S. Soares <kevin.soares@hotmail.com>
2023-04-16 15:12:39 -0300

    added BreakTime model

---
+++ Project_Appointments/Models/Attributes/BreakTimeValidationAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Appointments.Models.Attributes
+{
+    public class BreakTimeValidationAttribute : ValidationAttribute
+    {
+        private BreakTime model = default!;
+        public override bool IsValid(object? value)
+        {
+            if(value is not BreakTime)
+            {
+                ErrorMessage = "Invalid BreakTime";
+                return false;
+            }
+            model = (BreakTime) value;
+            return ArePropertiesValid();
+        }
+
+        private bool ArePropertiesValid()
+        {
+            return IsIdValid()
+                && IsScheduleIdValid()
+                && AreTimesValid();
+        }
+
+        private bool IsIdValid()
+        {
+            bool condition = model.Id >= 0L;
+            if(condition is false)
+            {
+                ErrorMessage = "Invalid BreakTime.Id";
+            }
+            return condition;
+        }
+
+        private bool IsScheduleIdValid()
+        {
+            bool condition = model.ScheduleId >= 0L;
+            if (condition is false)
+            {
+                ErrorMessage = "Invalid BreakTime.ScheduleId";
+            }
+            return condition;
+        }
+
+        private bool AreTimesValid()
+        {
+            bool condition = model.StartTime < model.EndTime;
+            if (condition is false)
+            {
+                ErrorMessage = "Invalid BreakTime.StartTime or BreakTime.EndTime";
+            }
+            return condition;
+        }
+    }
+}
---
+++ Project_Appointments/Models/BreakTime.cs
@@ -0,0 +1,25 @@
+﻿using Project_Appointments.Models.Attributes;
+
+namespace Project_Appointments.Models
+{
+    [BreakTimeValidation]
+    public class BreakTime
+    {
+        public long Id { get; set; }
+        public long ScheduleId { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+
+        public bool IsPartiallyInserted(BreakTime other)
+        {
+            return (other.StartTime <= StartTime && other.EndTime >= StartTime) 
+                || (other.StartTime <= EndTime && other.EndTime >= EndTime);
+        }
+
+        public bool IsCompletelyInserted(BreakTime other)
+        {
+            return (other.StartTime <= StartTime && other.EndTime >= StartTime)
+                && (other.StartTime <= EndTime && other.EndTime >= EndTime);
+        }
+    }
+}
