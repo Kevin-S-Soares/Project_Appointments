7f30433a8c9d3abcd30733985af0ab33ba73398e
Kevin S. Soares <kevin.soares@hotmail.com>
2023-04-19 17:33:37 -0300

    code more readable

--- Project_Appointments/Models/Services/Validators/ScheduleValidator.cs
+++ Project_Appointments/Models/Services/Validators/ScheduleValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Project_Appointments.Contexts;
 using Project_Appointments.Models.Exceptions;
+using System.Xml.Linq;
 
 namespace Project_Appointments.Models.Services.Validators
 {
@@ -13,35 +14,42 @@ namespace Project_Appointments.Models.Services.Validators
 
         public void Add(Schedule schedule)
         {
-            var structure = _context.Schedules
-                .Where(x => x.OdontologistId == schedule.OdontologistId)
-                .ToList();
-
-            foreach(var element in structure)
+            bool condition = IsWithinOtherSchedule(schedule);
+            if (condition is true)
             {
-                bool condition = TimeRepresentation.IsPartiallyInserted(schedule, element);
-                if(condition is true)
-                {
-                    throw new ModelException("Schedule overlaps other schedules");
-                }
+                throw new ModelException("Schedule overlaps other schedules");
             }
         }
 
         public void Update(Schedule schedule)
         {
+            bool condition = IsWithinOtherSchedule(schedule, isToUpdate: true);
+            if (condition is true)
+            {
+                throw new ModelException("Schedule overlaps other schedules");
+            }
+        }
+
+        private bool IsWithinOtherSchedule(Schedule schedule, bool isToUpdate = false)
+        {
             var structure = _context.Schedules
                 .Where(x => x.OdontologistId == schedule.OdontologistId)
                 .ToList();
 
-            structure.Remove(schedule);
+            if (isToUpdate)
+            {
+                structure.Remove(schedule);
+            }
+
             foreach (var element in structure)
             {
                 bool condition = TimeRepresentation.IsPartiallyInserted(schedule, element);
                 if (condition is true)
                 {
-                    throw new ModelException("Schedule overlaps other schedules");
+                    return true;
                 }
             }
+            return false;
         }
     }
 }
