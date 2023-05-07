ffcf3f7370529501be30fdfe2d95fd24dd25c7c5
Kevin S. Soares <kevin.soares@hotmail.com>
2023-05-07 13:51:10 -0300

    fixed appointment tests

--- ModelTests/Services/AppointmentServiceTests.cs
+++ ModelTests/Services/AppointmentServiceTests.cs
@@ -4,7 +4,10 @@ using Moq;
 using Project_Appointments.Contexts;
 using Project_Appointments.Models;
 using Project_Appointments.Models.Exceptions;
+using Project_Appointments.Services;
 using Project_Appointments.Services.AppointmentService;
+using Project_Appointments.Services.BreakTimeService;
+using Project_Appointments.Services.ScheduleService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,32 +18,28 @@ namespace ModelTests.Services
     public class AppointmentServiceTests
     {
         private AppointmentService _model = default!;
+        private Mock<IScheduleService> _mockScheduleService = default!;
 
-        private readonly IQueryable<BreakTime> _dataBreakTime = new List<BreakTime>()
+        private readonly BreakTime _dataBreakTime = new()
         {
-            new()
-            {
-                Id = 1L,
-                ScheduleId = 1L,
-                StartDay = DayOfWeek.Monday,
-                StartTime = new TimeSpan(12, 0, 0),
-                EndDay = DayOfWeek.Monday,
-                EndTime = new TimeSpan(13, 0, 0)
-            }
-        }.AsQueryable();
+            Id = 1L,
+            ScheduleId = 1L,
+            StartDay = DayOfWeek.Monday,
+            StartTime = new TimeSpan(12, 0, 0),
+            EndDay = DayOfWeek.Monday,
+            EndTime = new TimeSpan(13, 0, 0)
+        };
 
-        private readonly IQueryable<Schedule> _dataSchedule = new List<Schedule>()
+        private readonly Schedule _dataSchedule = new()
         {
-            new()
-            {
-                Id = 1L,
-                OdontologistId = 1L,
-                StartDay = DayOfWeek.Monday,
-                StartTime = new TimeSpan(9, 0, 0),
-                EndDay = DayOfWeek.Monday,
-                EndTime = new TimeSpan(21, 0, 0),
-            }
-        }.AsQueryable();
+            Id = 1L,
+            OdontologistId = 1L,
+            StartDay = DayOfWeek.Monday,
+            StartTime = new TimeSpan(9, 0, 0),
+            EndDay = DayOfWeek.Monday,
+            EndTime = new TimeSpan(21, 0, 0),
+        };
+
 
         private readonly IQueryable<Appointment> _dataAppointment = new List<Appointment>()
         {
@@ -58,25 +57,14 @@ namespace ModelTests.Services
         [TestInitialize]
         public void Setup()
         {
-            var mockBreakTimeSet = new Mock<DbSet<BreakTime>>();
-            mockBreakTimeSet.As<IQueryable<BreakTime>>().Setup(x => x.Provider)
-                .Returns(_dataBreakTime.Provider);
-            mockBreakTimeSet.As<IQueryable<BreakTime>>().Setup(x => x.Expression)
-                .Returns(_dataBreakTime.Expression);
-            mockBreakTimeSet.As<IQueryable<BreakTime>>().Setup(x => x.ElementType)
-                .Returns(_dataBreakTime.ElementType);
-            mockBreakTimeSet.As<IQueryable<BreakTime>>().Setup(x => x.GetEnumerator())
-                .Returns(_dataBreakTime.GetEnumerator());
+            _mockScheduleService = new Mock<IScheduleService>();
+            _mockScheduleService.Setup(x => x.FindById(It.IsAny<long>())).Returns(
+                new ServiceResponse<Schedule>(_dataSchedule, 200));
 
-            var mockScheduleSet = new Mock<DbSet<Schedule>>();
-            mockScheduleSet.As<IQueryable<Schedule>>().Setup(x => x.Provider)
-                .Returns(_dataSchedule.Provider);
-            mockScheduleSet.As<IQueryable<Schedule>>().Setup(x => x.Expression)
-                .Returns(_dataSchedule.Expression);
-            mockScheduleSet.As<IQueryable<Schedule>>().Setup(x => x.ElementType)
-                .Returns(_dataSchedule.ElementType);
-            mockScheduleSet.As<IQueryable<Schedule>>().Setup(x => x.GetEnumerator())
-                .Returns(_dataSchedule.GetEnumerator());
+            var mockBreakTimeService = new Mock<IBreakTimeService>();
+            mockBreakTimeService.Setup(x => x.FindAllFromSameSchedule(It.IsAny<Appointment>())).Returns(
+                new ServiceResponse<IEnumerable<BreakTime>>(
+                    new List<BreakTime>() { _dataBreakTime }, 200));
 
             var mockAppointmentSet = new Mock<DbSet<Appointment>>();
             mockAppointmentSet.As<IQueryable<Appointment>>().Setup(x => x.Provider)
@@ -89,11 +77,9 @@ namespace ModelTests.Services
                 .Returns(_dataAppointment.GetEnumerator());
 
             var mockContext = new Mock<ApplicationContext>();
-            mockContext.Setup(x => x.BreakTimes).Returns(mockBreakTimeSet.Object);
-            mockContext.Setup(x => x.Schedules).Returns(mockScheduleSet.Object);
             mockContext.Setup(x => x.Appointments).Returns(mockAppointmentSet.Object);
 
-            _model = new(mockContext.Object);
+            _model = new(mockContext.Object, mockBreakTimeService.Object, _mockScheduleService.Object);
         }
 
         private readonly Appointment _input_0 = new()
@@ -109,8 +95,9 @@ namespace ModelTests.Services
         [TestMethod]
         public void AddValidAppointment()
         {
-            _model.Add(_input_0);
-            Assert.IsTrue(true);
+            var result = _model.Create(_input_0);
+            Assert.AreEqual(expected: _input_0, actual: result.Data);
+            Assert.AreEqual(expected: 201, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_1 = new()
@@ -126,8 +113,12 @@ namespace ModelTests.Services
         [TestMethod]
         public void AddInvalidAppointment_0()
         {
-            Assert.ThrowsException<ServiceException>(() => _model.Add(_input_1),
-                "Invalid referred schedule");
+            _mockScheduleService.Setup(x => x.FindById(It.IsAny<long>())).Returns(
+                new ServiceResponse<Schedule>("Invalid referred schedule", 500));
+
+            var result = _model.Create(_input_1);
+            Assert.AreEqual(expected: "Invalid referred schedule", actual: result.ErrorMessage);
+            Assert.AreEqual(expected: 500, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_2 = new()
@@ -143,8 +134,10 @@ namespace ModelTests.Services
         [TestMethod]
         public void AddInvalidAppointment_1()
         {
-            Assert.ThrowsException<ServiceException>(() => _model.Add(_input_2),
-                "Appointment is not within its referred schedule");
+            var result = _model.Create(_input_2);
+            Assert.AreEqual(expected: "Appointment is not within its referred schedule", 
+                actual: result.ErrorMessage);
+            Assert.AreEqual(expected: 500, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_3 = new()
@@ -160,8 +153,10 @@ namespace ModelTests.Services
         [TestMethod]
         public void AddInvalidAppointment_2()
         {
-            Assert.ThrowsException<ServiceException>(() => _model.Add(_input_3),
-                "Appointment overlaps breakTimes");
+            var result = _model.Create(_input_3);
+            Assert.AreEqual(expected: "Appointment overlaps breakTimes",
+                actual: result.ErrorMessage);
+            Assert.AreEqual(expected: 500, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_4 = new()
@@ -177,8 +172,10 @@ namespace ModelTests.Services
         [TestMethod]
         public void AddInvalidAppointment_3()
         {
-            Assert.ThrowsException<ServiceException>(() => _model.Add(_input_4),
-                "Appointment overlaps other appointments");
+            var result = _model.Create(_input_4);
+            Assert.AreEqual(expected: "Appointment overlaps other appointments",
+                actual: result.ErrorMessage);
+            Assert.AreEqual(expected: 500, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_5 = new()
@@ -194,8 +191,9 @@ namespace ModelTests.Services
         [TestMethod]
         public void UpdateValidAppointment_0()
         {
-            _model.Update(_input_5);
-            Assert.IsTrue(true);
+            var result = _model.Update(_input_5);
+            Assert.AreEqual(expected: _input_5, actual: result.Data);
+            Assert.AreEqual(expected: 200, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_6 = new()
@@ -211,8 +209,12 @@ namespace ModelTests.Services
         [TestMethod]
         public void UpdateInvalidAppointment_0()
         {
-            Assert.ThrowsException<ServiceException>(() => _model.Update(_input_6),
-                "Invalid referred schedule");
+            _mockScheduleService.Setup(x => x.FindById(It.IsAny<long>())).Returns(
+                new ServiceResponse<Schedule>("Invalid referred schedule", 500));
+
+            var result = _model.Update(_input_6);
+            Assert.AreEqual(expected: "Invalid referred schedule", actual: result.ErrorMessage);
+            Assert.AreEqual(expected: 500, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_7 = new()
@@ -228,8 +230,10 @@ namespace ModelTests.Services
         [TestMethod]
         public void UpdateInvalidAppointment_1()
         {
-            Assert.ThrowsException<ServiceException>(() => _model.Update(_input_7),
-                "Appointment is not within its referred schedule");
+            var result = _model.Update(_input_7);
+            Assert.AreEqual(expected: "Appointment is not within its referred schedule",
+                actual: result.ErrorMessage);
+            Assert.AreEqual(expected: 500, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_8 = new()
@@ -245,8 +249,10 @@ namespace ModelTests.Services
         [TestMethod]
         public void UpdateInvalidAppointment_2()
         {
-            Assert.ThrowsException<ServiceException>(() => _model.Update(_input_8),
-                "Appointment overlaps breakTimes");
+            var result = _model.Update(_input_8);
+            Assert.AreEqual(expected: "Appointment overlaps breakTimes",
+                actual: result.ErrorMessage);
+            Assert.AreEqual(expected: 500, actual: result.StatusCode);
         }
 
         private readonly Appointment _input_9 = new()
@@ -262,8 +268,9 @@ namespace ModelTests.Services
         [TestMethod]
         public void UpdateValidAppointment_1()
         {
-            _model.Update(_input_9);
-            Assert.IsTrue(true);
+            var result = _model.Update(_input_9);
+            Assert.AreEqual(expected: _input_9, actual: result.Data);
+            Assert.AreEqual(expected: 200, actual: result.StatusCode);
         }
     }
 }
